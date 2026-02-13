341be16cac7453117a458f68e6be123346762ac9
Subdirección Nacional de Activos Fijos y Almacén <snafa.dna@funcionjudicial.gob.ec>
2026-02-13 05:03:15 +0000

    perdon las del firebase

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -7,13 +7,13 @@ import { getAnalytics, isSupported } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA-Bp7lMBIG4PhqxIy-qf9TWHZv3imOrBE",
-  authDomain: "prueba-fb-22d2a.firebaseapp.com",
-  projectId: "prueba-fb-22d2a",
-  storageBucket: "prueba-fb-22d2a.firebasestorage.app",
-  messagingSenderId: "313592397016",
-  appId: "1:313592397016:web:78410d7530c2f7a3aaabad",
-  measurementId: "G-5C741M9BMG"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 
