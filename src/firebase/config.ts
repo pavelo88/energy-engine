1eaf3a3b911fcb09124029fbebeaae0e550c9205
Subdirección Nacional de Activos Fijos y Almacén <snafa.dna@funcionjudicial.gob.ec>
2026-02-26 19:34:59 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7259724745-7c3be",
+  "appId": "1:471174429224:web:0d49c502849e533bee931d",
+  "apiKey": "AIzaSyDGhl-BlziGKIzf1Unx9W7xgrpexPEd2CE",
+  "authDomain": "studio-7259724745-7c3be.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "471174429224"
+};
